3b429bffb41e36d9f69f3f65534ed1987c7a0bdf
saumyalohia <32180197+saumyalohia@users.noreply.github.com>
2018-10-26 14:08:13 -0500

    Add files via upload

--- UtPod.cpp
+++ UtPod.cpp
@@ -2,3 +2,32 @@
 // Created by Saumya Lohia on 10/26/18.
 //
 
+#include "UtPod.h"
+
+using namespace std;
+
+UtPod(int size){}
+
+int addSong(Song const &s) {}
+
+
+int removeSong(Song const &s) {}
+
+
+void shuffle(){}
+
+
+void showSongList(){}
+
+
+void sortSongList(){}
+
+
+void clearMemory(){}
+
+
+int getTotalMemory() {
+    return memSize;
+}
+
+int getRemainingMemory{}
