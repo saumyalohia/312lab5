565f99e794f993c9ac297ad37b6238b2eaf4f8f9
Saumya Lohia <saumya.lohia@utexas.edu>
2018-10-31 23:40:55 -0500

    final version

--- UtPodDriver.cpp
+++ UtPodDriver.cpp
@@ -29,11 +29,12 @@ int main(int argc, char *argv[])
 
     Song s1("Panic! at the Disco", "Hey Look Ma I Made It", 51);
     Song s2("Bad Suns", "Salt", 5);
-    Song s3("Coldplay", "Viva la Vida", 6);
-    Song s4("Britney Spears", "Toxic", 5);
-    Song s5("HONNE", "Day 1", 456);
-    Song s6("Panic! at the Disco", "Old Fashioned", 6);
-
+    Song s3("Bad Suns", "Salt", 6);
+    Song s4("Coldplay", "Viva la Vida", 6);
+    Song s5("Britney Spears", "Toxic", 5);
+    Song s6("HONNE", "Day 1", 496);
+    Song s7("Panic! at the Disco", "Old Fashioned", 6);
+    Song s8("Panic! at the Disco", "Old Fashioned", 6);
 
     //total memory
     int mem = t.getTotalMemory();
@@ -44,11 +45,12 @@ int main(int argc, char *argv[])
     t.addSong(s2);
     t.addSong(s3);
     t.addSong(s4);
+    t.addSong(s5);
     t.showSongList();
     cout << "-------------------------------------------" << endl;
 
     //remove 1 song
-    t.removeSong(s4);
+    t.removeSong(s1);
     t.showSongList();
     cout << "-------------------------------------------" << endl;
 
@@ -57,12 +59,16 @@ int main(int argc, char *argv[])
     cout << "Remaining Memory: " << mem << endl;
 
     //try to add song 5 - should show error
-    int result = t.addSong(s5);
+    int result = t.addSong(s6);
     cout << "Result: " << result << endl;
 
-    //add song 6
-    result = t.addSong(s6);
+    //add remaining songs
+    result = t.addSong(s7);
     cout << "Result: " << result << endl;
+
+    result = t.addSong(s8);
+    cout << "Result: " << result << endl;
+
     t.showSongList();
     cout << "-------------------------------------------" << endl;
 
